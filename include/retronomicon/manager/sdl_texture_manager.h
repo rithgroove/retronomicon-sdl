9b39fb552b73e9364debb9176aaf0c2bae296485
rithgroove <rithgroove@gmail.com>
2025-12-05 06:10:19 +0700

    fix texture manager to handle font

--- include/retronomicon/manager/sdl_texture_manager.h
+++ include/retronomicon/manager/sdl_texture_manager.h
@@ -2,21 +2,31 @@
 
 #include <memory>
 #include <SDL.h>
+
 #include "retronomicon/graphics/texture.h"
 #include "retronomicon/manager/texture_manager.h"
-#include "retronomicon/asset/image_asset.h"  
+#include "retronomicon/asset/image_asset.h"
+#include "retronomicon/asset/font_asset.h"
 
-namespace retronomicon::sdl::manager{
-	using retronomicon::manager::TextureManager;
+namespace retronomicon::sdl::manager {
+
+    using retronomicon::manager::TextureManager;
     using retronomicon::graphics::Texture;
     using retronomicon::asset::ImageAsset;
-	class SDLTextureManager:public TextureManager{
-		public:
-			SDLTextureManager(SDL_Renderer* renderer);
-			~SDLTextureManager();
-			std::shared_ptr<Texture>  createTexture(std::shared_ptr<ImageAsset> ImageAsset) override;
-		private:
-			SDL_Renderer* m_renderer;
-	};
+    using retronomicon::asset::FontAsset;
 
-}
+    class SDLTextureManager : public TextureManager {
+    public:
+        SDLTextureManager(SDL_Renderer* renderer);
+        ~SDLTextureManager();
+
+        std::shared_ptr<Texture> createTexture(std::shared_ptr<ImageAsset> imageAsset) override;
+
+        // NEW: create texture from font atlas
+        std::shared_ptr<Texture> createTexture(std::shared_ptr<FontAsset> fontAsset) override;
+
+    private:
+        SDL_Renderer* m_renderer; // not owned
+    };
+
+} // namespace retronomicon::sdl::manager
--- src/manager/sdl_texture_manager.cpp
+++ src/manager/sdl_texture_manager.cpp
@@ -1,16 +1,52 @@
 #include "retronomicon/manager/sdl_texture_manager.h"
 #include "retronomicon/graphics/sdl_texture.h"
-namespace retronomicon::sdl::manager{
-	using retronomicon::sdl::graphics::SDLTexture;
-	SDLTextureManager::SDLTextureManager(SDL_Renderer* renderer): 
-		m_renderer(renderer)
-	{
+#include "retronomicon/asset/sdl_font_asset.h"
 
-	}
+namespace retronomicon::sdl::manager {
 
-	SDLTextureManager::~SDLTextureManager()= default;
+using retronomicon::sdl::graphics::SDLTexture;
 
-	std::shared_ptr<Texture>  SDLTextureManager::createTexture(std::shared_ptr<ImageAsset> imageAsset){
-		return std::make_shared<SDLTexture>(m_renderer,imageAsset);
-	}
+SDLTextureManager::SDLTextureManager(SDL_Renderer* renderer)
+    : m_renderer(renderer)
+{
 }
+
+SDLTextureManager::~SDLTextureManager() = default;
+
+std::shared_ptr<Texture>
+SDLTextureManager::createTexture(std::shared_ptr<ImageAsset> imageAsset) {
+    return std::make_shared<SDLTexture>(m_renderer, imageAsset);
+}
+
+// ---------------------------------------------------------------------------
+// NEW: Create an SDLTexture from a FontAsset atlas
+// ---------------------------------------------------------------------------
+std::shared_ptr<Texture>
+SDLTextureManager::createTexture(std::shared_ptr<FontAsset> fontAsset)
+{
+    // Downcast into backend implementation
+    auto sdlFont = std::dynamic_pointer_cast<retronomicon::sdl::asset::SDLFontAsset>(fontAsset);
+    if (!sdlFont) {
+        std::cerr << "[SDLTextureManager] ERROR: FontAsset is not SDLFontAsset.\n";
+        return nullptr;
+    }
+
+    SDL_Surface* atlasSurface = sdlFont->getAtlasSurface();
+    if (!atlasSurface) {
+        std::cerr << "[SDLTextureManager] ERROR: SDLFontAsset has no atlas surface.\n";
+        return nullptr;
+    }
+
+    // Create SDL_Texture from the atlas surface
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(m_renderer, atlasSurface);
+    if (!tex) {
+        std::cerr << "[SDLTextureManager] ERROR: Failed to create SDL_Texture: "
+                  << SDL_GetError() << "\n";
+        return nullptr;
+    }
+
+    // Wrap SDL_Texture inside SDLTexture (your backend texture class)
+    return std::make_shared<SDLTexture>(m_renderer, tex);
+}
+
+} // namespace retronomicon::sdl::manager
